7b8c643ae2c3b75d457cbf234b11514ac2c8d61b
rj <rj@rj,rj>
2026-02-21 14:03:22 -0500

    fix3

--- src/AvlCommand/Plugin.cs
+++ src/AvlCommand/Plugin.cs
@@ -1,10 +1,11 @@
 using BepInEx;
 using Jotunn.Utils;
-using UnityEngine;
+using Logger = Jotunn.Logger;
 
 namespace AvlCommand
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
     public class Plugin : BaseUnityPlugin
     {
         public const string ModGUID = "com.crzi.avlcommand";
